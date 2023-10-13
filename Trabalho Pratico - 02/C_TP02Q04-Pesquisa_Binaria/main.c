a0722b483e8e105f56aad94e500ed85f464293ad
Guilherme DeBrites <guilhermedebrites1@gmail.com>
2023-10-13 14:08:23 -0300

    Feat: Added log in question 04

--- Trabalho Pratico - 02/C_TP02Q04-Pesquisa_Binaria/main.c
+++ Trabalho Pratico - 02/C_TP02Q04-Pesquisa_Binaria/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+#include <time.h>
 
 #define MAX_LENGTH 100
 
-typedef struct {
+typedef struct
+{
     int id;
     char nome[MAX_LENGTH];
     int altura;
@@ -15,18 +18,22 @@ typedef struct {
     char estadoNascimento[MAX_LENGTH];
 } Jogador;
 
-void imprimir(Jogador *jogador){
+void imprimir(Jogador *jogador)
+{
     printf("[%i ## %s ## %i ## %i ## %i ## %s ## %s ## %s]\n", jogador->id, jogador->nome, jogador->altura, jogador->peso, jogador->anoNascimento, jogador->universidade, jogador->cidadeNascimento, jogador->estadoNascimento);
 }
 
-void replaceVirgula(char *str){
+void replaceVirgula(char *str)
+{
     int tamanho = strlen(str);
     char tmp[3 * tamanho];
-    int j = 0; 
+    int j = 0;
 
-    for (int i = 0; i < tamanho; i++) {
-        if (str[i] == ',' && str[i+1] == ',') {
-            tmp[j++] = ','; 
+    for (int i = 0; i < tamanho; i++)
+    {
+        if (str[i] == ',' && str[i + 1] == ',')
+        {
+            tmp[j++] = ',';
             tmp[j++] = 'n';
             tmp[j++] = 'a';
             tmp[j++] = 'o';
@@ -41,17 +48,18 @@ void replaceVirgula(char *str){
             tmp[j++] = 'd';
             tmp[j++] = 'o';
             tmp[j++] = ',';
-            
-            i++;
 
-        } else {
+            i++;
+        }
+        else
+        {
             tmp[j++] = str[i];
         }
-        
     }
 
-    if (tmp[j - 2] == ',') {
-        tmp[j-1] = 'n';
+    if (tmp[j - 2] == ',')
+    {
+        tmp[j - 1] = 'n';
         tmp[j++] = 'a';
         tmp[j++] = 'o';
         tmp[j++] = ' ';
@@ -70,8 +78,8 @@ void replaceVirgula(char *str){
     strcpy(str, tmp);
 }
 
-
-void clone(Jogador *jogador, Jogador *novo){
+void clone(Jogador *jogador, Jogador *novo)
+{
     novo->id = jogador->id;
     strcpy(novo->nome, jogador->nome);
     novo->altura = jogador->altura;
@@ -82,7 +90,8 @@ void clone(Jogador *jogador, Jogador *novo){
     strcpy(novo->estadoNascimento, jogador->estadoNascimento);
 }
 
-void ler(Jogador* jogador, char str[300]){
+void ler(Jogador *jogador, char str[300])
+{
     replaceVirgula(str);
     str[strcspn(str, "\n")] = '\0';
 
@@ -90,67 +99,137 @@ void ler(Jogador* jogador, char str[300]){
     token = strtok(str, ",");
     int i = 0;
 
-    while( token != NULL ) {
-        if(i % 8 == 0){
+    while (token != NULL)
+    {
+        if (i % 8 == 0)
+        {
             jogador->id = atoi(token);
-
-        }else if(i % 8 == 1){
+        }
+        else if (i % 8 == 1)
+        {
             strcpy(jogador->nome, token);
-
-        }else if(i % 8 == 2){
+        }
+        else if (i % 8 == 2)
+        {
             jogador->altura = atoi(token);
-
-        }else if(i % 8 == 3){
+        }
+        else if (i % 8 == 3)
+        {
             jogador->peso = atoi(token);
-
-        }else if(i % 8 == 4){
+        }
+        else if (i % 8 == 4)
+        {
             strcpy(jogador->universidade, token);
-
-        }else if(i % 8 == 5){
+        }
+        else if (i % 8 == 5)
+        {
             jogador->anoNascimento = atoi(token);
-
-        }else if(i % 8 == 6){
+        }
+        else if (i % 8 == 6)
+        {
             strcpy(jogador->cidadeNascimento, token);
-            
-
-        }else if(i % 8 == 7){
+        }
+        else if (i % 8 == 7)
+        {
             strcpy(jogador->estadoNascimento, token);
         }
         i++;
-        
+
         token = strtok(NULL, ",");
-   }
+    }
 }
 
-int main(){
+int pesquisaBinaria(Jogador *players, char *nome, int esq, int dir)
+{
+    if (esq > dir)
+        return -1;
+    else
+    {
+        int meio = (esq + dir) / 2;
+        if (strcmp(nome, players[meio].nome) == 0)
+        {
+            return 1;
+        }
+        else if (strcmp(nome, players[meio].nome) > 0)
+        {
+            return pesquisaBinaria(players, nome, meio + 1, dir);
+        }
+        else
+        {
+            return pesquisaBinaria(players, nome, esq, meio - 1);
+        }
+    }
+}
+
+int main()
+{
 
     Jogador players[3923];
     Jogador clonedPlayers[3923];
+    int contador = 0;
     char n[5];
 
-    FILE *arq = fopen("D:\\Algoritmos\\Aeds-II\\Trabalho Pratico - 02\\C_TP02Q04-Pesquisa_Binaria\\tmp\\players.csv", "r");
+    FILE *arq = fopen("/home/guilherme/Documentos/Aeds-II/Trabalho Pratico - 02/C_TP02Q04-Pesquisa_Binaria/tmp/players.csv", "r");
 
-    if(arq == NULL){
+    if (arq == NULL)
+    {
         printf("File not found\n");
         return 0;
     }
 
     char str[300];
-        fgets(str, sizeof(str), arq);
+    fgets(str, sizeof(str), arq);
     int i = 0;
-    while (fgets(str, sizeof(str), arq)) {
+    while (fgets(str, sizeof(str), arq))
+    {
         ler(&players[i], str);
         i++;
     }
 
-    getchar();
     scanf("%s", n);
-    while(strcmp(n, "FIM") != 0){
+    int j = 0;
+    while (strcmp(n, "FIM") != 0)
+    {
         int indice = atoi(n);
-        clone(&players[indice], &clonedPlayers[indice]);
+        clone(&players[indice], &clonedPlayers[contador++]);
+        j++;
         scanf("%s", n);
     }
-    fclose(arq);
 
+    for (int k = 0; k < j; k++)
+    {
+        for (int l = k + 1; l < j; l++)
+        {
+            if (strcmp(clonedPlayers[k].nome, clonedPlayers[l].nome) > 0)
+            {
+                Jogador tmp;
+                clone(&clonedPlayers[k], &tmp);
+                clone(&clonedPlayers[l], &clonedPlayers[k]);
+                clone(&tmp, &clonedPlayers[l]);
+            }
+        }
+    }
+
+    char name[100];
+    scanf(" %[^\n]s", name);
+
+    FILE *tempArq = fopen("808721_binaria.txt", "w");
+    int cmp = 0;
+    clock_t inicio, fim;
+    double total;
+
+    while (strcmp(name, "FIM") != 0)
+    {
+        inicio = clock();
+        printf("%s\n", pesquisaBinaria(clonedPlayers, name, 0, j - 1) == 1 ? "SIM" : "NAO");
+        fim = clock();
+        scanf(" %[^\n]s", name);
+    }
+
+    total = ((fim - inicio) / (double)CLOCKS_PER_SEC);
+    fprintf(tempArq, "808721\t%fs.\t%d", total, cmp);
+    fclose(tempArq);
+
+    fclose(arq);
     return 0;
 }
